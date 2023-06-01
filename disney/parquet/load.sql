9525c9ff51968dbd3a0436bc7ad756ebddcc3488
prasertcbs <bluecotton.k@gmail.com>
2023-06-01 08:48:37 +0700

    Delete disney directory

--- disney/parquet/load.sql
+++
@@ -1,4 +0,0 @@
-COPY director FROM 'disney\director.parquet' (FORMAT 'parquet');
-COPY dis_char FROM 'disney\dis_char.parquet' (FORMAT 'parquet');
-COPY movie_gross FROM 'disney\movie_gross.parquet' (FORMAT 'parquet');
-COPY voice_actor FROM 'disney\voice_actor.parquet' (FORMAT 'parquet');
