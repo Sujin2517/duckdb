13f5ae8b418246c33d93775d75bc7aaec241ece4
prasertcbs <bluecotton.k@gmail.com>
2023-06-01 08:15:56 +0700

    Add files via upload

---
+++ disney/parquet/schema.sql
@@ -0,0 +1,11 @@
+
+
+
+CREATE TABLE director(column0 VARCHAR, column1 VARCHAR);
+CREATE TABLE dis_char(column0 VARCHAR, column1 DATE, column2 VARCHAR, column3 VARCHAR, column4 VARCHAR);
+CREATE TABLE movie_gross(movie_title VARCHAR, release_date DATE, genre VARCHAR, mpaa_rating VARCHAR, total_gross BIGINT, inflation_adjusted_gross BIGINT);
+CREATE TABLE voice_actor("character" VARCHAR, voice_actor VARCHAR, movie VARCHAR);
+
+
+
+
