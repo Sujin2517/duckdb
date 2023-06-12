aff5742723be42e2c759c5c84c6c909ca80831b2
prasertcbs <bluecotton.k@gmail.com>
2023-06-12 10:19:37 +0700

    Update load.sql

--- coworkingspace/parquet/load.sql
+++ coworkingspace/parquet/load.sql
@@ -1,6 +1,6 @@
-COPY "User" FROM 'bbb\user.parquet' (FORMAT 'parquet');
-COPY Space FROM 'bbb\space.parquet' (FORMAT 'parquet');
-COPY Membership FROM 'bbb\membership.parquet' (FORMAT 'parquet');
-COPY Booking FROM 'bbb\booking.parquet' (FORMAT 'parquet');
-COPY Invoice FROM 'bbb\invoice.parquet' (FORMAT 'parquet');
-COPY "Event" FROM 'bbb\event.parquet' (FORMAT 'parquet');
+COPY "User" FROM 'parquet\user.parquet' (FORMAT 'parquet');
+COPY Space FROM 'parquet\space.parquet' (FORMAT 'parquet');
+COPY Membership FROM 'parquet\membership.parquet' (FORMAT 'parquet');
+COPY Booking FROM 'parquet\booking.parquet' (FORMAT 'parquet');
+COPY Invoice FROM 'parquet\invoice.parquet' (FORMAT 'parquet');
+COPY "Event" FROM 'parquet\event.parquet' (FORMAT 'parquet');
