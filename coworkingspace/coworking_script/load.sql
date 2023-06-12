95f58ca9938bc4484da4abf3f72942311fb1f1d6
prasertcbs <bluecotton.k@gmail.com>
2023-06-12 10:14:21 +0700

    Delete coworkingspace/coworking_script directory

--- coworkingspace/coworking_script/load.sql
+++
@@ -1,6 +0,0 @@
-COPY "User" FROM 'coworking\user.csv' (FORMAT 'csv', header 0, delimiter ',', quote '"');
-COPY Space FROM 'coworking\space.csv' (FORMAT 'csv', header 0, delimiter ',', quote '"');
-COPY Membership FROM 'coworking\membership.csv' (FORMAT 'csv', header 0, delimiter ',', quote '"');
-COPY Booking FROM 'coworking\booking.csv' (FORMAT 'csv', header 0, delimiter ',', quote '"');
-COPY Invoice FROM 'coworking\invoice.csv' (FORMAT 'csv', header 0, delimiter ',', quote '"');
-COPY "Event" FROM 'coworking\event.csv' (FORMAT 'csv', header 0, delimiter ',', quote '"');
