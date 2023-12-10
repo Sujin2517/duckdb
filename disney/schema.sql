cf97daf12e1ea0d48fa42ac2d8c991dfba0c9eef
prasertcbs <bluecotton.k@gmail.com>
2023-12-10 16:51:22 +0700

    Add files via upload

---
+++ disney/schema.sql
@@ -0,0 +1,11 @@
+
+
+
+CREATE TABLE director("name" VARCHAR, director VARCHAR);
+CREATE TABLE dis_char(movie_title VARCHAR, release_date DATE, hero VARCHAR, villian VARCHAR, song VARCHAR);
+CREATE TABLE movie_gross(movie_title VARCHAR, release_date DATE, genre VARCHAR, mpaa_rating VARCHAR, total_gross BIGINT, inflation_adjusted_gross BIGINT);
+CREATE TABLE voice_actor("character" VARCHAR, voice_actor VARCHAR, movie VARCHAR);
+
+
+
+
