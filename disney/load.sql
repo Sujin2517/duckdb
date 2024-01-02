2dd530ef7beead34a6391d178bb5249ab799001c
prasertcbs <bluecotton.k@gmail.com>
2024-01-02 14:21:47 +0700

    Add files via upload

--- disney/load.sql
+++ disney/load.sql
@@ -1,5 +1,5 @@
-COPY director FROM 'disney\director.parquet' (FORMAT 'parquet');
-COPY dis_char FROM 'disney\dis_char.parquet' (FORMAT 'parquet');
-COPY movie_gross FROM 'disney\movie_gross.parquet' (FORMAT 'parquet');
-COPY revenue FROM 'disney\revenue.parquet' (FORMAT 'parquet');
-COPY voice_actor FROM 'disney\voice_actor.parquet' (FORMAT 'parquet');
+COPY director FROM 'disney\director.csv' (FORMAT 'csv', header 1, delimiter ',', quote '"');
+COPY dis_char FROM 'disney\dis_char.csv' (FORMAT 'csv', header 1, delimiter ',', quote '"');
+COPY movie_gross FROM 'disney\movie_gross.csv' (FORMAT 'csv', header 1, delimiter ',', quote '"');
+COPY revenue FROM 'disney\revenue.csv' (FORMAT 'csv', header 1, delimiter ',', quote '"');
+COPY voice_actor FROM 'disney\voice_actor.csv' (FORMAT 'csv', header 1, delimiter ',', quote '"');
